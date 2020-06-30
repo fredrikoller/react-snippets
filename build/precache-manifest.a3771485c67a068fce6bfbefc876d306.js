a4737b7fae14ed1d3d38f5ffbbebf85f6ea5d847
fredrikoller <oller.fredrik@gmail.com>
2020-06-30 22:23:14 +0200

    added build. please work

---
+++ build/precache-manifest.a3771485c67a068fce6bfbefc876d306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6c7f9a2abdaa83a5a41124e21fd23d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8552d3f1c4fe6febeb1",
+    "url": "/static/css/main.7b03c521.chunk.css"
+  },
+  {
+    "revision": "e26995388c9ef3f54f6d",
+    "url": "/static/js/2.d4419fab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4419fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8552d3f1c4fe6febeb1",
+    "url": "/static/js/main.9cf0ea82.chunk.js"
+  },
+  {
+    "revision": "6526d045e4cc0d1b5bac",
+    "url": "/static/js/runtime-main.4e73649c.js"
+  }
+]);
